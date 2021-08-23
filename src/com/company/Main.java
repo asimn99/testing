bd88bf1ab4618f63834aa8485c08062fc6520818
asimn99 <asimnadeem584@gmail.com>
2021-08-23 15:31:55 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 49;
+        if (x == y) {
+            System.out.println("1");
+        } else if (x > y) {
+            System.out.println("2");
+        } else {
+            System.out.println("3");
+        }
+
+    }
+}
